349d5a5d419422d00585e7eb9007a23a9910e93f
Cristopher <cristopher.alarcon2201@alumnos.ubiobio.cl>
2025-04-27 00:05:12 -0400

    Update3

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,4 +1,4 @@
-//Crea un array con 5 frutas y recórrelo con un bucle for, mostrando cada fruta en consola.
+//!Crea un array con 5 frutas y recórrelo con un bucle for, mostrando cada fruta en consola.
 
 let array = ["Manzana", "Pera", "Sandia", "Naranja", "Frutilla" ];
 
